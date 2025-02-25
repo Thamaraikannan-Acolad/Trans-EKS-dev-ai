586e793964d72deff01831e641dabda54a2c910d
THAMARAIKANNAN TM <thamaraikannan2155@gmail.com>
2025-02-25 10:45:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,29 +3,29 @@ provider "aws" {
 }
 
 # VPC Creation
-resource "aws_vpc" "eks_vpc" {
+resource "aws_vpc" "trans_eks_vpc" {
   cidr_block = "10.0.0.0/16"
 }
 
 # Subnet 1
 resource "aws_subnet" "public1" {
-  vpc_id                  = aws_vpc.eks_vpc.id
+  vpc_id                  = aws_vpc.trans_eks_vpc.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = "eu-central-1a"
+  availability_zone       = "--------a"
 }
 
 # Subnet 2
 resource "aws_subnet" "public2" {
-  vpc_id                  = aws_vpc.eks_vpc.id
+  vpc_id                  = aws_vpc.trans_eks_vpc.id
   cidr_block              = "10.0.2.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = "eu-central-1b"
+  availability_zone       = "---------b"
 }
 
 # IAM Role for EKS Cluster
-resource "aws_iam_role" "eks_role" {
-  name = "eks-cluster-role"
+resource "aws_iam_role" "trans_eks_role" {
+  name = "trans-eks-cluster-role"
 
   assume_role_policy =<<EOF
 {
@@ -44,18 +44,18 @@ EOF
 }
 
 # Attach IAM policy to EKS Cluster Role
-resource "aws_iam_role_policy_attachment" "eks_policy" {
-  role       = aws_iam_role.eks_role.name
+resource "aws_iam_role_policy_attachment" "trans_eks_policy" {
+  role       = aws_iam_role.trans_eks_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 resource "aws_iam_role_policy_attachment"
-"eks_cloudwatch_policy"{
-  role   = aws_iam_role.eks_role.name
+"trans_eks_cloudwatch_policy"{
+  role   = aws_iam_role.trans_eks_role.name
   policy_arn = "--------"
 }
 # IAM Role for Node Group
-resource "aws_iam_role" "node_role" {
-  name = "eks-node-group-role"
+resource "aws_iam_role" "trans_node_role" {
+  name = "trans-eks-node-group-role"
 
   assume_role_policy = <<EOF
 {
@@ -75,19 +75,19 @@ EOF
 
 # Attach IAM policy to Node Role
 resource "aws_iam_role_policy_attachment" "node_policy" {
-  role       = aws_iam_role.node_role.name
+  role       = aws_iam_role.trans_node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 resource "aws_iam_role_policy_attachment"
-"node_cloudwatch_policy"{
-  role   = aws_iam_role.node_role.name
+"trans_node_cloudwatch_policy"{
+  role   = aws_iam_role.trans_node_role.name
   policy_arn = "--------"
 }
 
 # EKS Cluster Creation
 resource "trans_eks_cluster" "dev_ai" {
   name     = "dev-ai"
-  role_arn = aws_iam_role.eks_role.arn
+  role_arn = aws_iam_role.trans_eks_role.arn
 
   vpc_config {
     subnet_ids = [aws_subnet.public1.id, aws_subnet.public2.id]
@@ -95,14 +95,14 @@ resource "trans_eks_cluster" "dev_ai" {
 # Enable CloudWatch Logging  
 enabled_cluster_logs_types = ["api", "audit", "authenticator", "ControllerManager", "scheduler"]
 
-  depends_on = [aws_iam_role_policy_attachment.eks_policy, aws_iam_role_policy_attachment.eks_cloudwatch_policy]
+  depends_on = [aws_iam_role_policy_attachment.trans_eks_policy, aws_iam_role_policy_attachment.trans_eks_cloudwatch_policy]
 }
 
 # EKS Node Group Creation
 resource "aws_eks_node_group" "dev_ai_nodes" {
-  cluster_name    = aws_eks_cluster.dev_ai.name
+  cluster_name    = trans_eks_cluster.dev_ai.name
   node_group_name = "dev-ai-nodes"
-  node_role_arn   = aws_iam_role.node_role.arn
+  node_role_arn   = aws_iam_role.trans_node_role.arn
   subnet_ids      = [aws_subnet.public1.id, aws_subnet.public2.id]
   instance_types  = ["t3.medium"]
 
@@ -111,5 +111,5 @@ resource "aws_eks_node_group" "dev_ai_nodes" {
     min_size     = 1
     max_size     = 3
   }
-depends_on = [aws_iam_role_policy_attachment.node_policy, aws_iam_role_policy_attachment.node_cloudwatch_policy]
+depends_on = [aws_iam_role_policy_attachment.trans_node_policy, aws_iam_role_policy_attachment.trans_node_cloudwatch_policy]
 }
